fb6f6fa0c6da83f5e6d6822406df339246b59f0d
husnica <husnica@cwu.edu>
2026-04-22 19:31:23 -0700

    Add TestAnimal

---
+++ TestAnimal.java
@@ -0,0 +1,13 @@
+/**
+ * Test class for Rabbit
+ *
+ * @author Amir Husnic
+ */
+public class TestAnimal{
+    public static void main(String[] args) {
+        Rabbit rabbit1 = new Rabbit("Burger");
+        rabbit1.sound();
+        rabbit1.play();
+        rabbit1.eat();
+    }
+}
