19cfbccb7e743a0500ec445e11055ef3f0452e55
Thomas Velleff <you@example.com>
2026-04-22 22:40:57 -0700

    Tester class for Jaguar.java

---
+++ tester.java
@@ -0,0 +1,16 @@
+package CS380Lab2;
+
+public class tester {
+
+	
+	
+	public static void main(String[] args) {
+		
+		Animal myJaguar = new Jaguar("Franky");
+
+        myJaguar.sound(); 
+        myJaguar.play();  
+        myJaguar.eat();  
+		
+	}
+}
