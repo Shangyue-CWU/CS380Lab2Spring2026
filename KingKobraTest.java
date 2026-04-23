2e5e696782e5803c6233769f01a5af2f00537146
Greekypad <107791895+Greekypad@users.noreply.github.com>
2026-04-22 21:03:56 -0700

    Implement KingKobraTest

---
+++ KingKobraTest.java
@@ -0,0 +1,11 @@
+package project2_380;
+
+public class KingKobraTest {
+	public static void main(String[] args) {
+        KingKobra kobra = new KingKobra("William");
+        kobra.sound();
+        kobra.play();
+        kobra.eat();
+    }
+
+}
